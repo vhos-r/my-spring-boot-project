68e201c858f24b62bfec500803ca65f44fe51dd5
xiongchong <3298554262@qq.com>
2025-05-25 12:08:53 +0800

    test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,5 +6,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Nice to meet you !");
     }
 }
